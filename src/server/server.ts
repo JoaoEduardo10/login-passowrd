42984441d387e212275bc6ff865782a275a9ae04
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-19 09:56:58 -0300

    feat: add router of server

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,10 +2,12 @@ import "express-async-errors";
 import express from "express";
 import { globalErroMiddleware } from "./middlewares/global-error";
 import "dotenv/config";
+import { router } from "./router";
 
 const server = express();
 
 server.use(express.json());
-server.use(globalErroMiddleware);
+server.use(router);
 
+server.use(globalErroMiddleware);
 export { server };
