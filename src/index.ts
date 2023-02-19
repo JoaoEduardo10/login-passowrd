68247ae06d3d8deff2757559df3255abc926df15
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-19 09:10:27 -0300

    feat: add models of monogoose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("ok");
+import { server } from "./server/server";
+
+server.listen(8000, () => {
+  console.info("Servido iniciado");
+});
---
+++ src/server/models/mongo-models/User.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "../user";
+
+const User = model(
+  "Users",
+  new Schema<Omit<IUser, "id">>({
+    name: {
+      type: String,
+      required: true,
+      min: 3,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  })
+);
